6d076691a83f9d26230876dec77312ab8d2a8a45
lemonslice96 <lemonslice96@gmail.com>
2014-02-11 10:11:32 -0500

    4578

---
+++ problems/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("james Loveland");
+            Console.WriteLine("2585 Liberty St");
+            Console.WriteLine("Easton PA");
+        }
+    }
+}
